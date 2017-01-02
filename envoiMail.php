1bb9e5c471649936267f6b3a5129118f4b9a44b5
FFFFF <aude.falco@laposte.net>
2017-01-02 16:21:17 +0100

    test champs

--- envoiMail.php
+++ envoiMail.php
@@ -1,1 +1,31 @@
 <?php
+
+require_once ".init.php";
+require_once "fonction.php";
+
+
+if(isset($_FILES['file']))
+{ 
+     $dossier = 'upload/';
+     $fichier = basename($_FILES['avatar']['name']);
+     if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+     {
+          echo 'Upload effectué avec succès !';
+     }
+     else //Sinon (la fonction renvoie FALSE).
+     {
+          echo 'Echec de l\'upload !';
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/javaScript.js
@@ -0,0 +1,39 @@
+function surligne(champ, erreur)
+{
+	if(erreur)
+		champ.style.backgroundColor = "#fba";
+	else
+		champ.style.backgroundColor = "";
+}
+
+
+function verifierEmail(champ)
+{
+
+	console.log(champ.value);
+	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+	if(!regex.test(champ.value))
+	{
+		surligne(champ, true);
+		return false;
+	}
+	else
+	{
+		surligne(champ, false);
+		return true;
+	}
+}
+
+function verifier(f){
+
+	var verifEmail = verifierEmail(f.email);
+
+	if (verifEmail) {
+		return true;
+	}
+	else {
+		alert("Veuillez remplir correctement tous les champs");
+		return false;
+	}
+}
+
