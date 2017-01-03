ca4daeea1ae59718cc85c843eab6ce0e89346417
FFFFF <aude.falco@laposte.net>
2017-01-03 16:01:36 +0100

    email gen

--- envoiMail.php
+++ envoiMail.php
@@ -12,31 +12,40 @@ $sender=$_REQUEST['mailSender'];
 $receiver=$_REQUEST['mailReceiver'];
 $fileUpload=basename($_FILES['file']['name']);
 
-
 if (verifChamps($sender, $receiver, $fileUpload)) {
-     echo $sender;
-     echo $receiver;
-     echo $fileUpload;
-
-
+     
      if(isset($_FILES['file']))
      { 
-          $dossier = 'public/'.creeDossier();
+          $idDossier=creeDossier();
+          $dossierPublic = 'public/'.$idDossier;
           $fichier = basename($_FILES['file']['name']);
-     if(move_uploaded_file($_FILES['file']['tmp_name'], $dossier.'/'.$fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+     if(move_uploaded_file($_FILES['file']['tmp_name'], $dossierPublic.'/'.$fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
      {
+          inserChamps($idDossier,$sender,$receiver);
           echo 'Upload effectué avec succès !';
+          envoieMail($sender, $receiver,$idDossier);
      }
      else //Sinon (la fonction renvoie FALSE).
      {
-          echo 'Echec de l\'upload !';
+          echo 'Echec de l\'upload !'."\r\n";
      }
 }
 
 }
 else {
- echo 'Start again!';  
+    echo 'Start again!';  
 }
+
+/*header('location:../index.php?action=envoiMail&id='.$idDossier);*/
+
+
+
+
+
+
+
+
+
 
 
 
--- include/fonction.php
+++ include/fonction.php
@@ -1,25 +1,22 @@
 <?php
 
 /*------------------------------------------------------------------------*/
-	/*require les variables pour la connexion SQL dans le fichier .ini.php*/
+/*require les variables pour la connexion SQL dans le fichier .ini.php*/
 /*------------------------------------------------------------------------*/
 
 require_once ".init.php";
 
 
-
-
-
- /*------------------------------------------------------------------------*/
-				/*Class SQLpdo pour les requetes de bases*/
+/*------------------------------------------------------------------------*/
+/*Class SQLpdo pour les requetes de bases*/
 /*------------------------------------------------------------------------*/
 class SQLpdo {
 	function __construct(){
 
 		try {
-		    $this->db = new PDO('mysql:dbname='.DB.';host='.ADRESS.'',LOGIN,MDP);
+			$this->db = new PDO('mysql:dbname='.DB.';host='.ADRESS.'',LOGIN,MDP);
 		} catch (PDOException $e) {
-		    echo 'Connexion échouée : ' . $e->getMessage();
+			echo 'Connexion échouée : ' . $e->getMessage();
 		}
 
 	}
@@ -44,7 +41,7 @@ class SQLpdo {
 }
 
 /*------------------------------------------------------------------------*/
-					/*Exemple utilisation class SQLpdo*/
+/*Exemple utilisation class SQLpdo*/
 /*------------------------------------------------------------------------*/
 
 function insertImg($img,$textTop,$textBot,$clrTop=null,$clrBot=null,$sizeTop=null,$sizeBot=null,$id, $type){
@@ -78,7 +75,6 @@ function affichContDossier($idDossier){
 		}
 	}
 	return false;
-	
 }
 
 function creeDossier(){
@@ -91,78 +87,47 @@ function creeDossier(){
 	// doit être spécifié.
 
 	if (!mkdir($structure, 0777)) {
-	    die('Echec lors de la création du répertoire...');
+		die('Echec lors de la création du répertoire...');
 	}
 	return $idDossier;
 }
 
 function verifChamps($sender, $receiver, $fileUpload){
 
-if ($sender && $receiver && $fileUpload){
-return true;
-} else {
-return false;
-}
-}
-
-function envoieMail(){
-
-$nberreur=0;
-$nberreurmessage="Les champs erreur sont: \n";
-$serveur=false;
-
-$mail='aude.f@codeur.online';
-$sujet="Formulaire";
-$header='';
-$header.= 'From: aude <aude.f@codeur.online>'."\r\n";
-$header.='Reply-to:aude.f@codeur.online'."\r\n";
-$header.='X-Mailer: PHP/'.phpversion()."\r\n";
-$header.= 'Content-type: text/plain; charset=utf-8'."\r\n";
-
-ini_set('display_error','on');
-error_reporting (E_ALL);
-
-
-if($_REQUEST['Nom']==""){
-     $nberreur++;
-     $nberreurmessage=$nberreurmessage."nom \n";
-     $serveur=true;
-
+	if ($sender && $receiver && $fileUpload){
+		return true;
+	} else {
+		return false;
+	}
 }
 
-if(($_REQUEST['Email']=="") || (!filter_var($_REQUEST['Email'], FILTER_VALIDATE_EMAIL))){
-     $nberreur++;
-     $nberreurmessage=$nberreurmessage."\n email \n";
-     $serveur=true;
-}
+function envoieMail($sender, $receiver, $idDossier){
+	echo $sender."\r\n";
+	echo $receiver."\r\n";
 
+	$mail=$sender;
+	$sujet="Download file";
+	$header='';
+	$header.= 'From:'.$sender."\r\n";
+	$header.='Reply-to:KAPYBox'."\r\n";
+	$header.='X-Mailer: PHP/'.phpversion()."\r\n";
+	$header.= 'Content-type: text/plain; charset=utf-8'."\r\n";
 
-if ($serveur==true){
-     echo "Le nombre d'erreur est \n".$nberreurmessage."\n";
-     echo $nberreurmessage;
-}
+	ini_set('display_error','on');
+	error_reporting (E_ALL);      
 
-else {
+    //Envoi de l'e-mail
+	$dir = URL_SITE.'index.php?action=envoiMail&id='.$idDossier;
 
-          //Envoi de l'e-mail
-     $message='Le formulaire a été rempli. Voici les informations: 
-     Nom: '.$_REQUEST['Nom'].'
-     Courriel:'.$_REQUEST['Email'];
+	$message = "The file(s) has(ve) been updated, follow this link to download: \n\n $dir";
 
-
-     mail($mail,$sujet,$message,$header);
-
-}    
-
-
-
-
+	mail($mail,$sujet,$message,$header);
 }
 
 function inserChamps($idDossier,$mailEmetteur,$mailRecepteur){
-	if ($idDossier!=""){
+	if ($idDossier != ""){
 		$sql= new SQLpdo();
-		$idGen=$sql->insert("INSERT INTO `kypaLink` (`idDossier`, `mailEmetteur`, `mailRecepteur`) VALUES (':idDossier', ':mailEmetteur', ':mailRecepteur');",
-		array(":idDossier" => $idDossier,':mailEmetteur'=> $mailEmetteur, ':mailRecepteur'=> $mailRecepteur));
+		$idGen=$sql->insert("INSERT INTO `kypaLink` (idDossier, mailEmetteur, mailRecepteur) VALUES (:idDossier, :mailEmetteur, :mailRecepteur);",
+			array(":idDossier" => $idDossier,':mailEmetteur'=> $mailEmetteur, ':mailRecepteur'=> $mailRecepteur));
 	}
 }
